80dfc1bf8b29c3abb42547d836f78c487ab728fb
Ashish Alexander <ashishalex29@gmail.com>
2024-05-13 09:48:54 +0530

    backend server setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Profile from './pages//Profile'
 import Signin from './pages/Signin'
 import SignUp from './pages/Signup'
 import About from './pages/About'
+import Header from './components/Header'
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
