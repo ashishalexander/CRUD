7ca66b063aa2b8b1050fc591da3d4d75dbc33a4f
Ashish Alexander <ashishalex29@gmail.com>
2024-10-03 23:46:38 +0530

    error fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     <div>
       <Header />
       <div className='px-4 py-12 max-w-2xl mx-auto'>
-      <h1  className='text-3xl font-bold mb-4 text-slate-700 '>Welcome to my CRUD application</h1>
+      <h1  className='text-3xl font-bold mb-4 text-slate-700 '>Welcome to my CURD application</h1>
     </div>
     </div>
     
